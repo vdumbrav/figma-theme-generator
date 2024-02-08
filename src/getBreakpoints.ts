65da415d3e3320cef86be93d46a7402ba31064ef
GheorghiiNitaOneest <gheorghii.nita@oneest.com>
2024-02-08 14:57:11 +0200

    fix: #000: fix styletoCSS func and brecpoints generator

--- src/getBreakpoints.ts
+++ src/getBreakpoints.ts
@@ -3,10 +3,10 @@ export const getBreakpoints = (node?: Node) => {
   if (node && "children" in node) {
     return node.children.reduce((accum, current) => {
       if(+current.name.split(" ")[1] && current.type == "TEXT") {
-        return [...accum, current.name.split(" ")[1]];
+        return [...accum, { name: current.name.split(" ")[0], value: current.name.split(" ")[1] }];
       }
       return accum;
-    }, [] as string[]);
+    }, [] as {name: string, value: string}[]);
   } else {
     throw new Error("borders not found");
   }
