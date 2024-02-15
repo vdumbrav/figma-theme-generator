e8be21b088502a415736f15e1fb08dd74169881c
Vlad Dumbava <vlad@otherweb.com>
2024-02-15 18:44:58 +0200

    feat: add styleType - 'cssClass' | 'mixin'

--- index.ts
+++ index.ts
@@ -11,174 +11,181 @@ import { getSvgImgs } from "./src/getSvgImgs";
 import { getBreakpoints } from "./src/getBreakpoints";
 
 type Params = {
+  /**
+   * File ID for styles
+   */
+  figmaFileId: string;
+  /**
+   * Container ID for colors
+   */
+  colorsNodeId: string;
+  /**
+   * Container ID for spacings
+   */
+  spacingsNodeId: string;
+  /**
+   * Container ID for border radii
+   */
+  bordersNodeId: string;
+  /**
+   * Container ID for breakpoints
+   */
+  breakpointsNodeId?: string;
+  /**
+   * Container ID for monochrome SVG icons
+   */
+  iconsNodeId: string;
+  /**
+   * Container ID for typography
+   */
+  typographyNodeId: string;
+  /**
+   * Container ID for PNG icons
+   */
+  pngImgNodeId: string;
+  /**
+   * Container ID for SVG icons
+   */
+  svgImgNodeId: string;
+  /**
+   * Path to place the generated spacings, borders, typography, and colors for iOS in the project
+   */
+  themePath?: string;
+  /**
+   * Path to place the generated light theme colors for Android in the project
+   */
+  androidLightPath?: string;
+  /**
+   * Path to place the generated dark theme colors for Android in the project
+   */
+  androidDarkPath?: string;
+  /**
+   * Path to place the generated SCSS styles in the project
+   */
+  cssPath?: string;
+  /**
+   * Path to place the generated monochrome icons in the project
+   */
+  iconsPath?: string;
+  /**
+   * Path to place the generated images in the project
+   */
+  imgPath?: string;
+  /**
+   * Path to place the generated SVG icons in the project
+   */
+  svgPath?: string;
+  /**
+   * List icons that should be generated as PNG
+   */
+  pngIcons?: {
     /**
-     * File ID for styles
+     * Figma icon node id
      */
-    figmaFileId: string;
+    id: string;
     /**
-     * Container ID for colors
+     * Just for identification
      */
-    colorsNodeId: string;
-    /**
-     * Container ID for spacings
-     */
-    spacingsNodeId: string;
-    /**
-     * Container ID for border radii
-     */
-    bordersNodeId: string;
-    /**
-     * Container ID for breakpoints
-     */
-    breakpointsNodeId?: string;
-    /**
-     * Container ID for monochrome SVG icons
-     */
-    iconsNodeId: string;
-    /**
-     * Container ID for typography
-     */
-    typographyNodeId: string;
-    /**
-     * Container ID for PNG icons
-     */
-    pngImgNodeId: string;
-    /**
-     * Container ID for SVG icons
-     */
-    svgImgNodeId: string;
-    /**
-     * Path to place the generated spacings, borders, typography, and colors for iOS in the project
-     */
-    themePath?: string;
-    /**
-     * Path to place the generated light theme colors for Android in the project
-     */
-    androidLightPath?: string;
-    /**
-     * Path to place the generated dark theme colors for Android in the project
-     */
-    androidDarkPath?: string;
-    /**
-     * Path to place the generated SCSS styles in the project
-     */
-    cssPath?: string;
-    /**
-     * Path to place the generated monochrome icons in the project
-     */
-    iconsPath?: string;
-    /**
-     * Path to place the generated images in the project
-     */
-    imgPath?: string;
-    /**
-     * Path to place the generated SVG icons in the project
-     */
-    svgPath?: string;
-    /**
-     * List icons that should be generated as PNG
-     */
-    pngIcons?: {
-        /**
-         * Figma icon node id
-         */
-        id: string;
-        /**
-         * Just for identification
-         */
-        description: string;
-    }[];
-    /**
-     * Path to env file with figma token
-     */
-    envPath?: string;
+    description: string;
+  }[];
+  /**
+   * Path to env file with figma token
+   */
+  envPath?: string;
+  /**
+   * Defines how styles should be applied. Options are 'cssClass' for direct CSS class application
+   * or 'mixin' for generating and using SCSS mixins.
+   */
+  styleType?: 'cssClass' | 'mixin';
 };
 
 export async function generate({
-                                   figmaFileId,
-                                   colorsNodeId,
-                                   spacingsNodeId,
-                                   bordersNodeId,
-                                   breakpointsNodeId,
-                                   iconsNodeId,
-                                   typographyNodeId,
-                                   pngImgNodeId,
-                                   svgImgNodeId,
-                                   themePath,
-                                   androidLightPath,
-                                   androidDarkPath,
-                                   cssPath,
-                                   iconsPath,
-                                   imgPath,
-                                   svgPath,
-                                   pngIcons,
-                                   envPath,
-                               }: Params) {
-    const figmaToken = config({ path: envPath || ".env.local" }).parsed?.[
-        "FIGMA_TOKEN"
-        ];
-    if (!figmaToken) {
-        throw new Error("figma token not found");
-    }
-    const apis = createApi({
-        personalAccessToken: figmaToken,
-        fileKey: figmaFileId,
-    });
-    console.log("fetch figma file");
-    const ids = [
-        colorsNodeId,
-        spacingsNodeId,
-        bordersNodeId,
-        iconsNodeId,
-        typographyNodeId,
-        pngImgNodeId,
-        svgImgNodeId,
-    ];
-    const nodes = await apis.getFileNodes({
-        fileKey: figmaFileId,
-        ids: breakpointsNodeId ? [...ids, breakpointsNodeId] : ids,
-    });
-    const colors = getColors(nodes.nodes[colorsNodeId]?.document);
-    const spacings = getSpacings(nodes.nodes[spacingsNodeId]?.document);
-    const borders = getBorders(nodes.nodes[bordersNodeId]?.document);
-    const typography = getTypography(nodes.nodes[typographyNodeId]);
-    const breakpoints = breakpointsNodeId ? getBreakpoints(nodes.nodes[breakpointsNodeId]?.document) : undefined;
-    console.log("create images");
-    await Promise.all([
-        getIcons(apis, iconsPath, pngIcons, nodes.nodes[iconsNodeId]?.document),
-        getPngImgs(apis, imgPath, nodes.nodes[pngImgNodeId]?.document),
-        getSvgImgs(apis, svgPath, nodes.nodes[svgImgNodeId]?.document),
-    ]);
+  figmaFileId,
+  colorsNodeId,
+  spacingsNodeId,
+  bordersNodeId,
+  breakpointsNodeId,
+  iconsNodeId,
+  typographyNodeId,
+  pngImgNodeId,
+  svgImgNodeId,
+  themePath,
+  androidLightPath,
+  androidDarkPath,
+  cssPath,
+  iconsPath,
+  imgPath,
+  svgPath,
+  pngIcons,
+  envPath,
+  styleType,
+}: Params) {
+  const figmaToken = config({ path: envPath || ".env.local" }).parsed?.[
+    "FIGMA_TOKEN"
+  ];
+  if (!figmaToken) {
+    throw new Error("figma token not found");
+  }
+  const apis = createApi({
+    personalAccessToken: figmaToken,
+    fileKey: figmaFileId,
+  });
+  console.log("fetch figma file");
+  const ids = [
+    colorsNodeId,
+    spacingsNodeId,
+    bordersNodeId,
+    iconsNodeId,
+    typographyNodeId,
+    pngImgNodeId,
+    svgImgNodeId,
+  ];
+  const nodes = await apis.getFileNodes({
+    fileKey: figmaFileId,
+    ids: breakpointsNodeId ? [...ids, breakpointsNodeId] : ids,
+  });
+  const colors = getColors(nodes.nodes[colorsNodeId]?.document);
+  const spacings = getSpacings(nodes.nodes[spacingsNodeId]?.document);
+  const borders = getBorders(nodes.nodes[bordersNodeId]?.document);
+  const typography = getTypography(nodes.nodes[typographyNodeId]);
+  const breakpoints = breakpointsNodeId ? getBreakpoints(nodes.nodes[breakpointsNodeId]?.document) : undefined;
+  console.log("create images");
+  await Promise.all([
+    getIcons(apis, iconsPath, pngIcons, nodes.nodes[iconsNodeId]?.document),
+    getPngImgs(apis, imgPath, nodes.nodes[pngImgNodeId]?.document),
+    getSvgImgs(apis, svgPath, nodes.nodes[svgImgNodeId]?.document),
+  ]);
 
-    console.log("write files");
-    await createThemeFile(spacings, borders, typography, breakpointsNodeId ? undefined : colors, themePath);
-    await createAndroidFiles(colors, androidLightPath, androidDarkPath);
-    await createCssFile(colors, spacings, borders, typography, breakpoints, cssPath);
-    console.log("success");
+  console.log("write files");
+  await createThemeFile(spacings, borders, typography, breakpointsNodeId ? undefined : colors, themePath);
+  await createAndroidFiles(colors, androidLightPath, androidDarkPath);
+  await createCssFile(colors, spacings, borders, typography, breakpoints, cssPath, styleType);
+  console.log("success");
 }
 
 const createThemeFile = async (
-    spacings: ReturnType<typeof getSpacings>,
-    borders: ReturnType<typeof getBorders>,
-    typography: ReturnType<typeof getTypography>,
-    colors?: ReturnType<typeof getColors>,
-    path?: string,
+  spacings: ReturnType<typeof getSpacings>,
+  borders: ReturnType<typeof getBorders>,
+  typography: ReturnType<typeof getTypography>,
+  colors?: ReturnType<typeof getColors>,
+  path?: string,
+  styleType?: 'cssClass' | 'mixin',
 ) => {
-    if (path) {
-        const file = `import {Platform, DynamicColorIOS, PlatformColor} from "react-native";
+  if (path) {
+    const file = `import {Platform, DynamicColorIOS, PlatformColor} from "react-native";
 const isIos = Platform.OS === "ios";
 export const theme = {
   colors: {
     transparent: "transparent",
     ${colors
-            ?.map(
-                (el) =>
-                    `${el.name} : isIos ? DynamicColorIOS({
+        ?.map(
+          (el) =>
+            `${el.name} : isIos ? DynamicColorIOS({
       light: "${el.light}",
       dark: "${el.dark}"
     }) : PlatformColor("@color/${el.name}"), `,
-            )
-            .join("\n    ")}
+        )
+        .join("\n    ")}
   },
   spacings: {
     ${spacings.map((el) => `${el.name}: ${el.value},`).join("\n    ")}
@@ -188,78 +195,77 @@ export const theme = {
   },
   typography: {
     ${typography
-            .map(
+        .map(
+          (el) =>
+            `${el.name}: {\n      ${Object.entries(el.value!)
+              .map(
                 (el) =>
-                    `${el.name}: {\n      ${Object.entries(el.value!)
-                        .map(
-                            (el) =>
-                                `${el[0]}: ${typeof el[1] === "string" ? `"${el[1]}"` : el[1]}`,
-                        )
-                        .join(",\n      ")}\n    },`,
-            )
-            .join("\n    ")}
+                  `${el[0]}: ${typeof el[1] === "string" ? `"${el[1]}"` : el[1]}`,
+              )
+              .join(",\n      ")}\n    },`,
+        )
+        .join("\n    ")}
   }
 } as const`;
-        await createFile(path, file);
-    }
+    await createFile(path, file);
+  }
 };
 
 const createAndroidFiles = async (
-    colors: ReturnType<typeof getColors>,
-    light?: string,
-    dark?: string,
+  colors: ReturnType<typeof getColors>,
+  light?: string,
+  dark?: string,
 ) => {
-    if (light) {
-        const file = `<?xml version="1.0" encoding="utf-8"?>
+  if (light) {
+    const file = `<?xml version="1.0" encoding="utf-8"?>
   <resources>
     <color name="primary_dark">#FFFFFF</color>
 
     ${colors
-            .map(
-                (el) =>
-                    `<color name="${el.name}">${
-                        el.light.length > 7
-                            ? "#" + el.light.slice(-2) + el.light.slice(1, 7)
-                            : el.light
-                    }</color>`,
-            )
-            .join("\n    ")}
+        .map(
+          (el) =>
+            `<color name="${el.name}">${el.light.length > 7
+              ? "#" + el.light.slice(-2) + el.light.slice(1, 7)
+              : el.light
+            }</color>`,
+        )
+        .join("\n    ")}
   </resources>`;
 
-        await createFile(light, file);
-    }
+    await createFile(light, file);
+  }
 
-    if (dark) {
-        const file = `<?xml version="1.0" encoding="utf-8"?>
+  if (dark) {
+    const file = `<?xml version="1.0" encoding="utf-8"?>
   <resources>
     <color name="primary_dark">#000000</color>
 
     ${colors
-            .map(
-                (el) =>
-                    `<color name="${el.name}">${
-                        el.dark.length > 7
-                            ? "#" + el.dark.slice(-2) + el.dark.slice(1, 7)
-                            : el.dark
-                    }</color>`,
-            )
-            .join("\n    ")}
+        .map(
+          (el) =>
+            `<color name="${el.name}">${el.dark.length > 7
+              ? "#" + el.dark.slice(-2) + el.dark.slice(1, 7)
+              : el.dark
+            }</color>`,
+        )
+        .join("\n    ")}
   </resources>`;
 
-        await createFile(dark, file);
-    }
+    await createFile(dark, file);
+  }
 };
 
 const createCssFile = async (
-    colors: ReturnType<typeof getColors>,
-    spacings: ReturnType<typeof getSpacings>,
-    borders: ReturnType<typeof getBorders>,
-    typography: ReturnType<typeof getTypography>,
-    breakpoints?: ReturnType<typeof getBreakpoints>,
-    path?: string,
+  colors: ReturnType<typeof getColors>,
+  spacings: ReturnType<typeof getSpacings>,
+  borders: ReturnType<typeof getBorders>,
+  typography: ReturnType<typeof getTypography>,
+  breakpoints?: ReturnType<typeof getBreakpoints>,
+  path?: string,
+  styleType: 'cssClass' | 'mixin' = 'cssClass'
 ) => {
-    if (path) {
-const file = `
+  if (path) {
+    const file = `
 @use "sass:map";
 
 ${colors.map((el) => `$${el.name}: ${el.light}`).join(";\n")};
@@ -327,30 +333,35 @@ ${borders.map((el) => `$${el.name}: ${el.value}px`).join(";\n")};
 ${breakpoints?.map((el) => `$${el.name}: ${el.value}px`).join(";\n")};
 
 ${typography
-            .map((el) => `.${el.name} {\n  ${styleToCss(el.value)}\n}`)
-            .join(";\n")};
+  .map((el) => {
+      const cssContent = styleToCss(el.value);
+      return styleType === 'mixin'
+          ? `@mixin ${el.name} {\n  ${cssContent}\n}`
+          : `.${el.name} {\n  ${cssContent}\n}`;
+  })
+  .join(styleType === 'mixin' ? "\n" : ";\n") + (styleType !== 'mixin' ? ';' : '')};
 `;
 
-        await createFile(path, file);
-    }
+    await createFile(path, file);
+  }
 };
 
 const styleToCss = (style: Style) => {
-    return Object.entries(style)
-        .map(([k, v]) => {
-            const key = k as keyof Style;
-            switch (key) {
-                case "fontFamily":
-                    return `font-family: var(--${`${v}`.toLowerCase()});`;
-                case "fontSize":
-                    return `font-size: ${v}px;`;
-                case "fontWeight":
-                    return `font-weight: ${v};`;
-                case "letterSpacing":
-                    return `letter-spacing: ${v}px;`;
-                case "lineHeight":
-                    return `line-height: ${v}px;`;
-            }
-        })
-        .join("\n  ");
+  return Object.entries(style)
+    .map(([k, v]) => {
+      const key = k as keyof Style;
+      switch (key) {
+        case "fontFamily":
+          return `font-family: var(--${`${v}`.toLowerCase()});`;
+        case "fontSize":
+          return `font-size: ${v}px;`;
+        case "fontWeight":
+          return `font-weight: ${v};`;
+        case "letterSpacing":
+          return `letter-spacing: ${v}px;`;
+        case "lineHeight":
+          return `line-height: ${v}px;`;
+      }
+    })
+    .join("\n  ");
 };
